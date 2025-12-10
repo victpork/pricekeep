45860f57c5e877dcd87f5451a5d4f674ad27b009
victpork <224617+victpork@users.noreply.github.com>
2025-12-11 00:47:11 +1300

    Serve static files outside project directory

---
+++ src/main/java/io/equalink/pricekeep/StaticResource.java
@@ -0,0 +1,22 @@
+package io.equalink.pricekeep;
+
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.FileSystemAccess;
+import io.vertx.ext.web.handler.StaticHandler;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+public class StaticResource {
+
+    @ConfigProperty(name = "fetchImgPath")
+    String imgDir;
+
+
+    void installRoute(@Observes Router router) {
+        // Configure the route to serve static files from a specific path
+        router.route("/static/assets/img/*")
+            .handler(StaticHandler.create(FileSystemAccess.ROOT, imgDir)); // The absolute path to your external directory
+    }
+}
