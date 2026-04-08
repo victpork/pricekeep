2c1300ed42739f0f474b0b433f12fcbcf23ef700
victpork <224617+victpork@users.noreply.github.com>
2026-04-08 17:04:24 +1200

    Fix Input Quote UI inconsistencies

--- src/main/webui/src/views/QuoteForm.vue
+++ src/main/webui/src/views/QuoteForm.vue
@@ -10,6 +10,7 @@ import {
     FieldDescription,
     FieldError,
     FieldGroup,
+    FieldLabel
 } from '@/components/ui/field'
 import { Input } from '@/components/ui/input'
 import {
@@ -21,6 +22,12 @@ import {
     SelectGroup,
     SelectLabel,
 } from '@/components/ui/select'
+import {
+    InputGroup,
+    InputGroupAddon,
+    InputGroupText,
+    InputGroupInput,
+} from '@/components/ui/input-group'
 import Switch from '@/components/ui/switch/Switch.vue'
 import Label from '@/components/ui/label/Label.vue'
 import { usePostApiProductProductIdQuote } from '@/apiClient'
@@ -33,6 +40,7 @@ import Storepicker from '@/components/StorePicker/Storepicker.vue'
 const props = defineProps<{
     productId: number
     productName: string
+    price: number
 }>()
 
 const emit = defineEmits<{
@@ -42,16 +50,16 @@ const emit = defineEmits<{
 const { mutateAsync: postQuote } = usePostApiProductProductIdQuote()
 
 const formSchema = z.object({
-    price: z.number().min(0),
+    price: z.coerce.number().min(0.01),
     quoteDate: z.custom<DateValue>(),
     storeId: z.number(),
     discountType: z.enum([Type.PERCENTAGE, Type.FIXED_AMOUNT, Type.BUNDLE, Type.NEAR_EXPIRY, Type.OTHER]).optional(),
-    salePrice: z.number().min(0).optional(),
-    multibuyQuantity: z.number().min(0).optional(),
+    salePrice: z.coerce.number().min(0).optional(),
+    multibuyQuantity: z.coerce.number().min(0).optional(),
 })
 
 const defaultValues: z.input<typeof formSchema> = {
-    price: 1.00,
+    price: props.price,
     quoteDate: today(getLocalTimeZone()),
     storeId: 0,
     discountType: undefined,
@@ -68,14 +76,14 @@ const form = useForm({
         postQuote({
             productId: props.productId,
             data: {
-                price: value.price,
+                price: Number(value.price),
                 quoteDate: value.quoteDate.toString(),
                 storeInfo: {
                     id: value.storeId,
                 },
                 discountType: value.discountType,
-                discountPrice: value.salePrice,
-                multibuyQuantity: value.multibuyQuantity,
+                discountPrice: value.salePrice ? Number(value.salePrice) : undefined,
+                multibuyQuantity: value.multibuyQuantity ? Number(value.multibuyQuantity) : undefined,
             }
         }).then((p) => {
             console.log('Posted', p.status)
@@ -116,100 +124,110 @@ watch(
 const discountFormEnabled = ref(false)
 </script>
 <template>
-    <DialogContent class="min-w-[800px]">
+    <DialogContent class="min-w-auto">
         <DialogHeader>
             <DialogTitle>Quote</DialogTitle>
-            <DialogDescription class="text-sm">
+            <DialogDescription>
                 Quote price for {{ capitalise(props.productName) }}
             </DialogDescription>
         </DialogHeader>
-        <form id="quoteForm" @submit.prevent="form.handleSubmit" class="flex flex-col gap-6">
-            <div class="flex flex-row gap-6">
+        <form id="quoteForm" @submit.prevent="form.handleSubmit" class="flex flex-col">
+            <div class="flex flex-col w-full max-w-sm items-center space-x-2 px-1 gap-6">
                 <FieldGroup>
                     <form.Field name="quoteDate" v-slot="{ field }">
                         <Field :data-invalid="isInvalid(field)">
+                            <FieldLabel :for="field.name">Date for Quote</FieldLabel>
                             <DatePicker :model-value="field.state.value"
                                 @update:model-value="(v) => field.handleChange(v as DateValue)" placeholder="Quote Date"
                                 :max-value="today(getLocalTimeZone())" />
                             <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
                         </Field>
                     </form.Field>
+                    <form.Field name="storeId" v-slot="{ field }">
+                        <Field :data-invalid="isInvalid(field)">
+                            <FieldLabel :for="field.name">Store</FieldLabel>
+                            <Storepicker :model-value="field.state.value"
+                                @update:model-value="(v) => field.handleChange(v as number)" />
+                            <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
+                        </Field>
+                    </form.Field>
+                    <form.Field name="price" v-slot="{ field }">
+                        <Field :data-invalid="isInvalid(field)" class="w-[200px]">
+                            <FieldLabel :for="field.name">Original Price</FieldLabel>
+                            <InputGroup>
+                                <InputGroupAddon>
+                                    <InputGroupText>$</InputGroupText>
+                                </InputGroupAddon>
+                                <InputGroupInput type="number" step="0.01" placeholder="0.00" id="price" :name="field.name"
+                                    :model-value="field.state.value" @blur="field.handleBlur"
+                                    @input="field.handleChange($event.target.value)" />
+                            </InputGroup>
+                            <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
+                        </Field>
+                    </form.Field>
                 </FieldGroup>
-                <form.Field name="storeId" v-slot="{ field }">
-                    <Field :data-invalid="isInvalid(field)">
-                        <Storepicker :model-value="field.state.value"
-                            @update:model-value="(v) => field.handleChange(v as number)" />
-                        <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
-                    </Field>
-                </form.Field>
+                <FieldGroup>
+                    <div class="flex items-center space-x-2">
+                        <Switch id="airplane-mode" v-model="discountFormEnabled" />
+                        <Label for="airplane-mode">Discount</Label>
+                    </div>
+                    <Transition name="fade" enter-from-class="opacity-0"
+                        enter-active-class="transition duration-500 ease-out" leave-to-class="opacity-0"
+                        leave-active-class="transition duration-500 ease-in">
+                        <FieldGroup v-if="discountFormEnabled">
+                            <form.Field name="discountType" v-slot="{ field }">
+                                <Field :data-invalid="isInvalid(field)">
+                                    <FieldLabel :for="field.name">Discount Type</FieldLabel>
+                                    <Select :name="field.name" :model-value="field.state.value"
+                                        @update:model-value="(e) => field.handleChange(e as Type)">
+                                        <SelectTrigger class="w-[180px]">
+                                            <SelectValue placeholder="Discount Type" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                <SelectLabel class="px-2">Discount Type</SelectLabel>
+                                                <SelectItem v-for="discountType in Type" :key="discountType"
+                                                    :value="discountType">
+                                                    {{ skewToCapText(discountType) }}
+                                                </SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+                                    <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
+                                </Field>
+                            </form.Field>
+                            <form.Field name="salePrice" v-slot="{ field }">
+                                <Field :data-invalid="isInvalid(field)">
+                                    <FieldLabel :for="field.name">Price after Discount</FieldLabel>
+                                    <InputGroup>
+                                        <InputGroupAddon>
+                                            <InputGroupText>$</InputGroupText>
+                                        </InputGroupAddon>
+                                        <InputGroupInput type="number" step="0.01" placeholder="0.00" id="sale-price" :name="field.name"
+                                            :model-value="field.state.value" @blur="field.handleBlur"
+                                            @input="field.handleChange($event.target.value)" />
+                                    </InputGroup>
+                                    <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
+                                </Field>
+                            </form.Field>
+                            <Transition name="fade" enter-from-class="opacity-0"
+                                enter-active-class="transition duration-500 ease-out" leave-to-class="opacity-0"
+                                leave-active-class="transition duration-500 ease-in">
+                                <div v-if="form.state.values.discountType === Type.BUNDLE">
+                                    <form.Field name="multibuyQuantity" v-slot="{ field }">
+                                        <Field :data-invalid="isInvalid(field)">
+                                            <Input v-model="field.state.value" type="number"
+                                                placeholder="Multibuy Quantity" />
+                                            <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
+                                        </Field>
+                                    </form.Field>
+                                </div>
+                            </Transition>
+                        </FieldGroup>
+                    </Transition>
+                </FieldGroup>
             </div>
-            <div>
-                <form.Field name="price" v-slot="{ field }">
-                    <Field :data-invalid="isInvalid(field)" class="w-[200px]">
-                        <Input id="price" :name="field.name" :model-value="field.state.value" @blur="field.handleBlur"
-                            @input="field.handleChange($event.target.valueAsNumber)" type="number" placeholder="Price"
-                            min="0" step="0.01" class="w-[200px]" />
-                        <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
-                        <FieldDescription>Original price without discount</FieldDescription>
-                    </Field>
-                </form.Field>
-            </div>
-            <div>
-                <div class="flex items-center space-x-2 py-2">
-                    <Switch id="airplane-mode" v-model="discountFormEnabled" />
-                    <Label for="airplane-mode">Discount</Label>
-                </div>
-                <Transition name="fade" enter-from-class="opacity-0"
-                    enter-active-class="transition duration-500 ease-out" leave-to-class="opacity-0"
-                    leave-active-class="transition duration-500 ease-in">
-                    <FieldGroup v-if="discountFormEnabled">
-                        <form.Field name="discountType" v-slot="{ field }">
-                            <Field :data-invalid="isInvalid(field)">
-                                <Select :name="field.name" :model-value="field.state.value"
-                                    @update:model-value="(e) => field.handleChange(e as Type)">
-                                    <SelectTrigger class="w-[180px]">
-                                        <SelectValue placeholder="Discount Type" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectGroup>
-                                            <SelectLabel class="px-2">Discount Type</SelectLabel>
-                                            <SelectItem v-for="discountType in Type" :key="discountType"
-                                                :value="discountType">
-                                                {{ skewToCapText(discountType) }}
-                                            </SelectItem>
-                                        </SelectGroup>
-                                    </SelectContent>
-                                </Select>
-                                <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
-                                <FieldDescription>Original price for the product without discount/promotion
-                                </FieldDescription>
-                            </Field>
-                        </form.Field>
-                        <form.Field name="salePrice" v-slot="{ field }">
-                            <Field :data-invalid="isInvalid(field)">
-                                <Input v-model="field.state.value" type="number" placeholder="Sale Price" />
-                                <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
-                                <FieldDescription>Sale price for the product with discount/promotion</FieldDescription>
-                            </Field>
-                        </form.Field>
-                        <Transition name="fade" enter-from-class="opacity-0"
-                            enter-active-class="transition duration-500 ease-out" leave-to-class="opacity-0"
-                            leave-active-class="transition duration-500 ease-in">
-                            <div v-if="form.state.values.discountType === Type.BUNDLE">
-                                <form.Field name="multibuyQuantity" v-slot="{ field }">
-                                    <Field :data-invalid="isInvalid(field)">
-                                        <Input v-model="field.state.value" type="number"
-                                            placeholder="Multibuy Quantity" />
-                                        <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
-                                        <FieldDescription>Multibuy quantity for the product with discount/promotion
-                                        </FieldDescription>
-                                    </Field>
-                                </form.Field>
-                            </div>
-                        </Transition>
-                    </FieldGroup>
-                </Transition>
-            </div>
+
         </form>
         <DialogFooter>
             <DialogClose as-child>
